9c3d221773061d4fcc5a3f8dea4f2995aa20ee18
ig-rib <igrib98@gmail.com>
2021-03-22 12:40:19 -0300

    [Semana 1] Updated Makeover Monday page

--- infovis/src/views/homework/semana1/MakeoverMonday.vue
+++ infovis/src/views/homework/semana1/MakeoverMonday.vue
@@ -1,24 +1,8 @@
 <template>
     <v-container>
-        <v-carousel height="auto">
-            <v-carousel-item src="@/assets/semana1/makeoverMonday/2003.png"></v-carousel-item>
-            <v-carousel-item src="@/assets/semana1/makeoverMonday/2004.png"></v-carousel-item>
-            <v-carousel-item src="@/assets/semana1/makeoverMonday/2005.png"></v-carousel-item>
-            <v-carousel-item src="@/assets/semana1/makeoverMonday/2006.png"></v-carousel-item>
-            <v-carousel-item src="@/assets/semana1/makeoverMonday/2007.png"></v-carousel-item>
-            <v-carousel-item src="@/assets/semana1/makeoverMonday/2008.png"></v-carousel-item>
-            <v-carousel-item src="@/assets/semana1/makeoverMonday/2009.png"></v-carousel-item>
-            <v-carousel-item src="@/assets/semana1/makeoverMonday/2010.png"></v-carousel-item>
-            <v-carousel-item src="@/assets/semana1/makeoverMonday/2011.png"></v-carousel-item>
-            <v-carousel-item src="@/assets/semana1/makeoverMonday/2012.png"></v-carousel-item>
-            <v-carousel-item src="@/assets/semana1/makeoverMonday/2013.png"></v-carousel-item>
-            <v-carousel-item src="@/assets/semana1/makeoverMonday/2014.png"></v-carousel-item>
-            <v-carousel-item src="@/assets/semana1/makeoverMonday/2015.png"></v-carousel-item>
-            <v-carousel-item src="@/assets/semana1/makeoverMonday/2016.png"></v-carousel-item>
-            <v-carousel-item src="@/assets/semana1/makeoverMonday/2017.png"></v-carousel-item>
-            <v-carousel-item src="@/assets/semana1/makeoverMonday/2018.png"></v-carousel-item>
-            <v-carousel-item src="@/assets/semana1/makeoverMonday/2019.png"></v-carousel-item>
-        </v-carousel>
+        <Tableau
+            :url="'https://public.tableau.com/shared/QH6GQGX8F?:display_count=y&:origin=viz_share_link'">
+        </Tableau>
     </v-container>
 </template>
 
